c9a9ed9315112aa3f01ce7ccf64ac260b0470c5c
James Parrish <jthomasparrishjr@gmail.com>
2022-09-13 19:44:36 -0500

    lab complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+    const timer = setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000);
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
